5e6d5d75f554987383dad94987609283bfddaa04
trang17 <34981161+trang17@users.noreply.github.com>
2022-02-05 15:51:32 +0700

    added bloging

---
+++ src/pages/blog/index.js
@@ -0,0 +1,39 @@
+import * as React from 'react'
+import { Link, graphql } from 'gatsby'
+import Layout from '../../components/layout'
+
+const BlogPage = ({ data }) => {
+  return (
+    <Layout pageTitle="Trang | Blog">
+      <h1>I write about random stuffs that interest me.</h1>
+      {
+        data.allMdx.nodes.map(node => (
+          <article key={node.id}>
+            <h2>
+              <Link to={`/blog/${node.slug}`}>
+                {node.frontmatter.title}
+              </Link>
+            </h2>
+          </article>
+        ))
+      }
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query {
+    allMdx(sort: {fields: frontmatter___date, order: DESC}) {
+      nodes {
+        frontmatter {
+          date
+          title
+        }
+        id
+        slug
+      }
+    }
+  }
+`
+
+export default BlogPage
