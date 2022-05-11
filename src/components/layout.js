b94053a16b6bc44717231647e6f7fee4c438722a
Trang Nguyen <34981161+trang17@users.noreply.github.com>
2022-05-11 14:25:19 +0700

    contents change

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,47 +1,51 @@
-import * as React from 'react'
-import { Link } from 'gatsby'
+import * as React from "react"
+import { Link } from "gatsby"
 import {
   container,
   navLinks,
   navLinkItem,
   navLinkText,
-} from './layout.module.css'
+} from "./layout.module.css"
 
 const Layout = ({ pageTitle, children }) => {
   return (
-    <div className={container}>
-      <title>{pageTitle}</title>
-      <nav>
-        <ul className={navLinks}>
-          <li className={navLinkItem}>
-            <Link to="/" className={navLinkText} activeClassname="active">
-              Projects
-            </Link>
-          </li>
-          <li className={navLinkItem}>
-            <Link to="/study-notes" className={navLinkText} activeClassname="active">
-              Study Notes
-            </Link>
-          </li>
-
-          <li className={navLinkItem}>
-            <Link to="/blog" className={navLinkText} activeClassname="active">
-              Blog
-            </Link>
-          </li>
-
-          <li className={navLinkItem}>
-            <Link to="/contact" className={navLinkText}>
-              Contact
-            </Link>
-          </li>
-        </ul>
-      </nav>
-      <main>
-        {children}
-      </main>
-    </div>
+      <div className={container}>
+        <title>{pageTitle}</title>
+        <nav>
+          <ul className={navLinks}>
+            <li className={navLinkItem}>
+              <Link
+                to="/"
+                className={navLinkText}
+                activeClassname="active"
+              >
+                Projects
+              </Link>
+            </li>
+            <li className={navLinkItem}>
+              <Link
+                to="/study-notes"
+                className={navLinkText}
+                activeClassname="active"
+              >
+                Study notes
+              </Link>
+            </li>
+            <li className={navLinkItem}>
+              <Link to="/blog" className={navLinkText} activeClassname="active">
+                Blog
+              </Link>
+            </li>
+            <li className={navLinkItem}>
+              <Link to="/contact" className={navLinkText}>
+                Contact
+              </Link>
+            </li>
+          </ul>
+        </nav>
+        <main>{children}</main>
+      </div>
   )
 }
 
-export default Layout 
+export default Layout
